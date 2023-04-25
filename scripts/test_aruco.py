135dfa689f868fb5dd8f9ba48fe8cd7a6f43c33d
Niharika Desaraju <niharikadesaraju@Niharikas-MacBook-Pro-2.local>
2023-04-25 14:51:35 -0400

    adding local test aruco code

--- scripts/test_aruco.py
+++ scripts/test_aruco.py
@@ -5,6 +5,7 @@ import argparse
 import imutils
 import time
 import sys
+import matplotlib.pyplot as plt
 
 
 def get_aruco_distance():
@@ -15,20 +16,22 @@ def get_aruco_distance():
 
     print("[INFO] starting video stream...")
     vs = VideoStream(src=0).start()
-    time.sleep(10.0)
+    time.sleep(2.0)
 
     while True:
     # grab the frame from the threaded video stream and resize it
     # to have a maximum width of 1000 pixels
         frame = vs.read()
         frame = imutils.resize(frame, width=1000)
+
         cv2.imshow("Frame", frame)
+        cv2.waitKey(0)
 
         # detect ArUco markers in the input frame
         (corners, ids, rejected_img_points) = cv2.aruco.detectMarkers(frame,
             aruco_dict, parameters=aruco_params)
 
-    
+        print("ids found: {0}".format(ids))
 
     # Detect the markers in the image
     # corners, ids, rejected_img_points = cv2.aruco.detectMarkers(self.cv_image, aruco_dict, parameters=aruco_params)
@@ -38,17 +41,23 @@ def get_aruco_distance():
         if len(corners) > 0:
             table_p_c = np.array([0, 0, 0])
             # Loop over all detected markers
+            all_markers = [100, 200, 300, 400, 500, 600]
             for i in range(len(ids)):
-                if (i == 0): # IDK ABOUT THIS @CAROLYNE
+                if (ids[i] == 600): # IDK ABOUT THIS @CAROLYNE
                     # Access the corners of the ith detected marker
                     marker_corners = corners[i][0]
                     
                     # Compute the center of the marker
                     marker_center = np.mean(marker_corners, axis=0)
                     
+                    camera_info_width = 640
+                    camera_info_height = 480
+                    D = [0.011434452409443142, -0.06828667970669487, -0.0019653945487388834, 0.0049774108597738205, 0.0]
+                    dist_coeffs = D
+                    K = [520.9903919408287, 0.0, 339.3749038438434, 0.0, 520.3005424418425, 238.66492050564054, 0.0, 0.0, 1.0]
                     # Convert the center point to a normalized ray in camera coordinates
-                    cx = camera_info.width / 2.0
-                    cy = camera_info.height / 2.0
+                    cx = camera_info_width / 2.0
+                    cy = camera_info_height / 2.0
                     fx = K[0, 0]
                     fy = K[1, 1]
                     x_c = (marker_center[0] - cx) / fx
@@ -58,6 +67,11 @@ def get_aruco_distance():
                     
                     # Convert the point to 3D coordinates in the camera frame
                     table_p_c = np.array([x_c, y_c, z_c])
+
+                    plt.imshow(img)
+                    plt.plot(marker_center)
+                    plt.show()
+
                     
                 if (i == item):
                     # Calculate distance
